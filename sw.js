28cdfae6251dce250537554c553a5d072e897b33
ahmedafmm-web <ahmed.af.mm@gmail.com>
2026-07-05 04:10:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "contractor-v1";
+const CACHE_NAME = "contractor-v2"; // 
 const ASSETS = [
   "./index.html",
   "./app.js",
@@ -8,14 +8,33 @@ const ASSETS = [
   "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
 ];
 
+// عند التثبيت
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting(); //
 });
 
+// 
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+//
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
